66c85077cd8dfd10caf0d83ec4769810a2085bd2
”Anton2-7” <labunskii@gmail.com>
2022-08-04 21:33:37 +0500

    v 0.3
    Added input

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import Styles from './Home.modules.css';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+
 
 const Home = () => (
-  <div>Home</div>
+  <div className='Main__wrapper'><Typography
+  className='Main_title'
+  variant="h6"
+  color="inherit"
+  component="div"
+  sx={{ flexGrow: 2 }}
+  >
+  Библиотека
+  </Typography>
+  <Typography
+  className='input__name'
+  variant="h6"
+  color="inherit"
+  component="div"
+  sx={{ flexGrow: 2 }}
+  >
+  Название поля
+  </Typography>
+  <div className="input__field">
+    <div className="input-text"><TextField id="outlined-basic" label="Outlined" size="small" variant="outlined" multiline={true} />
+    </div>
+  </div>
+</div>
+
+
+
 );
+
 
 export default Home;
