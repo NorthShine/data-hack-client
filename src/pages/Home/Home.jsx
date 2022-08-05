5c44533b7162576d4eaa74cecbe18ac9ee4269be
”Anton2-7” <labunskii@gmail.com>
2022-08-05 22:24:04 +0500

    v 0.3

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
-import Styles from './Home.modules.css';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Styles from './Home.modules.css';
 
-
-const Home = () => (
-  <div className='Main__wrapper'><Typography
-  className='Main_title'
-  variant="h6"
-  color="inherit"
-  component="div"
-  sx={{ flexGrow: 2 }}
-  >
-  Библиотека
-  </Typography>
-  <Typography
-  className='input__name'
-  variant="h6"
-  color="inherit"
-  component="div"
-  sx={{ flexGrow: 2 }}
-  >
-  Название поля
-  </Typography>
-  <div className="input__field">
-    <div className="input-text"><TextField id="outlined-basic" label="Outlined" size="small" variant="outlined" multiline={true} />
+function Home() {
+  return (
+    <div className="Main__wrapper">
+      <Typography
+        className="Main_title"
+        variant="h6"
+        color="inherit"
+        component="div"
+        sx={{ flexGrow: 2 }}
+      >
+        Библиотека
+      </Typography>
+      <Typography
+        className="input__name"
+        variant="h6"
+        color="inherit"
+        component="div"
+        sx={{ flexGrow: 2 }}
+      >
+        Название поля
+      </Typography>
+      <div className="input__field">
+        <div className="input-text">
+          <TextField id="outlined-basic" label="Outlined" size="small" variant="outlined" multiline />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-
-
-);
-
+  );
+}
 
 export default Home;
