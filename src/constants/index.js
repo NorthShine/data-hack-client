0213137413097b0c3aced3847a68aab83fc46bb5
Paul <52328887+paulbraam@users.noreply.github.com>
2022-08-07 13:30:40 +0300

    Update data types

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ export const DEFAULT_ALERT_POSITION = {
 export const DEFAULT_ALERT_TIMEOUT = 3000;
 
 export const DATA_TYPES = [
-  'int', 'str', 'datetime', 'float', 'bool'
+  'int', 'str', 'float', 'bool'
 ];
 
 export const DEFAULT_LANGUAGES = [
